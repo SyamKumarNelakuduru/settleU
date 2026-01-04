40484fda675deb0a46174e1e14944b3904210648
Syam Kumar <syamkumar@Syams-MacBook-Air.local>
2026-01-03 20:28:23 -0600

    saved data from firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,9 @@
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID" // Optional
-  }
+export const firebaseConfig = {
+  apiKey: "AIzaSyB153w0zuMZOM57OExz7QKQVyogAP4u-fk",
+  authDomain: "settleu.firebaseapp.com",
+  projectId: "settleu",
+  storageBucket: "settleu.firebasestorage.app",
+  messagingSenderId: "596561367560",
+  appId: "1:596561367560:web:367bad53389cd3d52477bd",
+  measurementId: "G-42HMR8FQFV"
 };
