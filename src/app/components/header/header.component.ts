9f0a55946bb51195c6cef8a7ab8f91f5cb86b4b1
Samuel sujith Kondeti <samuelsujithkondeti@Samuels-MacBook-Air.local>
2026-01-03 21:17:04 -0600

    work done on search icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { SearchComponent } from '../search/search.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   appName = 'Lewis University';
+  isSearchOpen = false;
 
   ngOnInit(): void {
     console.log('Header initialized with app name:', this.appName);
@@ -21,8 +23,27 @@ export class HeaderComponent implements OnInit {
     console.log('Search clicked');
   }
 
+  openSearch(): void {
+    this.isSearchOpen = true;
+  }
+
+  closeSearch(): void {
+    this.isSearchOpen = false;
+  }
+
+  handleSearch(query: string): void {
+    console.log('Search submitted:', query);
+    // Perform any search handling here or navigate to search results
+    this.closeSearch();
+  }
+
   onLoginClick(): void {
     // TODO: Navigate to login page or open login modal
     console.log('Login clicked');
+  }
+
+  @HostListener('window:keydown.escape')
+  onEscape(): void {
+    if (this.isSearchOpen) this.closeSearch();
   }
 }
