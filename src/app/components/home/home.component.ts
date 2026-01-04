4cc60a9398a3ced1456558d2d0980ecd4aa8acf5
Syam Kumar <syamkumar@Syams-MacBook-Air.local>
2026-01-03 19:20:36 -0600

    added header components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [
+    HeaderComponent
+  ]
 })
 export class HomeComponent {}
